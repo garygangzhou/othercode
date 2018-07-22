8f9cf1d7e68eb1b3ed0a643cf6a74e6b98cf1b0f
garygangzhou <gary.gangzhou@gmail.com>
2018-07-22 10:56:46 -0400

    add AD query project

---
+++ ad_query/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace AD_Query {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLogID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LogID";
+            // 
+            // txtLogID
+            // 
+            this.txtLogID.Location = new System.Drawing.Point(54, 19);
+            this.txtLogID.Name = "txtLogID";
+            this.txtLogID.Size = new System.Drawing.Size(231, 20);
+            this.txtLogID.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(185, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Search On CCODS.cancercare.on.ca";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(332, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtResults
+            // 
+            this.txtResults.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResults.CausesValidation = false;
+            this.txtResults.Location = new System.Drawing.Point(12, 105);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.ReadOnly = true;
+            this.txtResults.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResults.Size = new System.Drawing.Size(586, 410);
+            this.txtResults.TabIndex = 4;
+            this.txtResults.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 515);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLogID);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "LDAP search";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLogID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtResults;
+    }
+}
+
---
+++ ad_query/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.DirectoryServices;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AD_Query {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            txtResults.Text = string.Empty;
+
+            string logid = txtLogID.Text.Trim();
+            if (String.IsNullOrWhiteSpace(logid)) {
+                txtResults.Text = "Please enter logon id.";
+                return;
+            }
+            try { 
+
+            //perform a search
+            string ldapBindingStr = @"LDAP://helloworld.com/DC=hellods,DC=world,DC=com"; 
+            DirectoryEntry ldapConnection = new DirectoryEntry(ldapBindingStr);
+            DirectorySearcher search = new DirectorySearcher(ldapConnection);
+            search.ReferralChasing = ReferralChasingOption.All;
+
+            search.Filter = "(samaccountName=" + logid + ")"; // user's logon id  gzhou
+            // search.Filter = "(cn=" + username + ")";  // this is full name, zhou, gary
+
+            SearchResult result = search.FindOne();
+
+            if (result != null) {             
+                ResultPropertyCollection fields = result.Properties;
+
+                foreach (String ldapField in fields.PropertyNames) {
+                        //*************** output all properties  *********************************************************************
+                        // cycle through objects in each field e.g. group membership  
+                        // (for many fields there will only be one object such as name)
+                        foreach (Object myCollection in fields[ldapField]) {
+                            if (ldapField.Equals("accountexpires")) {
+                                if ("9223372036854775807" == fields["accountexpires"][0].ToString()) // this is magic default value
+                                    txtResults.Text = txtResults.Text + (String.Format(OutputFormatStr, ldapField, "never expire.")) + Environment.NewLine;
+                                else                                    
+                                    txtResults.Text = txtResults.Text + (String.Format(OutputFormatStr, ldapField, DateTime.FromFileTime(long.Parse(fields["accountexpires"][0].ToString())))) + Environment.NewLine;
+                            }
+                            else if (ldapField.Equals("pwdlastset"))
+                                txtResults.Text = txtResults.Text + (String.Format(OutputFormatStr, ldapField, DateTime.FromFileTime(long.Parse(fields["pwdlastset"][0].ToString()))))  + Environment.NewLine;
+                            else if (ldapField.Equals("objectguid"))
+                                txtResults.Text = txtResults.Text + (String.Format(OutputFormatStr, ldapField, new Guid((byte[])myCollection))) + Environment.NewLine;
+                            else
+                                txtResults.Text = txtResults.Text + (String.Format(OutputFormatStr, ldapField, myCollection.ToString())) + Environment.NewLine;
+                        }
+                }
+            }
+            else {
+                // user does not exist  
+                txtResults.Text = "User not found!";
+            }
+            }
+            catch (Exception ex){
+                txtResults.Text = ex.Message;
+            }
+        }
+
+        private string OutputFormatStr {
+            get { return "{0,-20} : {1}"; }
+        }
+    }
+}
